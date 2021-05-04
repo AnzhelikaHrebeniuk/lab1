2c4bb050dc57a8b0c34e784094b92ab967f1f7d0
AnzhelikaHrebeniuk <48449969+AnzhelikaHrebeniuk@users.noreply.github.com>
2021-05-04 19:11:19 +0300

    Update ExceptionManager.java

--- ExceptionManager.java
+++ ExceptionManager.java
@@ -1,25 +1,31 @@
 import java.util.List;
 
+//cтворюємо клас
 public class ExceptionManager {
 	
+	//ініціалізуємо лічильники та List
 	private final List<Class<? extends Exception>> criticalExceptions;
 	private long criticalExceptionsCounter;
 	private long usualExceptionsCounter;
 	
+	//створюємо конструктор
 	public ExceptionManager(List<Class<? extends Exception>> criticalExceptions) {
 		this.criticalExceptions = criticalExceptions;
 	}
 	
+	//створюємо метод, який перевіряє на критичність
 	public boolean isCritical(Exception exception) {
 		Class<?> exceptionClass = exception.getClass();
 		for (var criticalExceptionClass : criticalExceptions) {
-			if (criticalExceptionClass.isAssignableFrom(exceptionClass)) {
+			if (criticalExceptionClass.isAssignableFrom(exceptionClass)) { 
 				return true;
 			}
 		}
 		
 		return false;
 	}
+	
+	//збільшення лічильників
 	
 	public boolean checkAndIncrementCounter(Exception exception) {
 		boolean isCritical = isCritical(exception);
